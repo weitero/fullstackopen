bee423b53c307e5ce11b5d6d5d0d70edb31e94a4
weitero <112836181+weitero@users.noreply.github.com>
2026-04-03 23:23:03 +0100

    refactor: add Content component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,7 +1,23 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </div>
   );
 };
@@ -18,15 +34,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
