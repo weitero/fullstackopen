eaf3a597ba2bc96e0e6d48c85536b67aad3eb669
weitero <112836181+weitero@users.noreply.github.com>
2026-04-05 23:43:36 +0100

    refactor: statistics is extracted into its own component; simplify code

--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 - [x] 1.5: Course Information, step 5
 - [x] 1.6: unicafe step 1
 - [x] 1.7: unicafe step 2
-- [ ] 1.8: unicafe step 3
+- [x] 1.8: unicafe step 3
 - [ ] 1.9: unicafe step 4
 - [ ] 1.10: unicafe step 5
 - [ ] 1.11*: unicafe step 6
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,38 +1,39 @@
 import { useState } from "react";
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Display = ({ feedback, count }) => {
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  const total = good + neutral + bad;
   return (
-    <p>
-      {feedback} {count}
-    </p>
+    <div>
+      <Display feedback={"good"} count={good} />
+      <Display feedback={"neutral"} count={neutral} />
+      <Display feedback={"bad"} count={bad} />
+      <Display feedback={"all"} count={total} />
+      <Display feedback={"average"} count={(good - bad) / total} />
+      <Display feedback={"positive"} count={100 * (good / total) + " %"} />
+    </div>
   );
 };
+
+const Display = ({ feedback, count }) => (
+  <p>
+    {feedback} {count}
+  </p>
+);
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
 
-  const handleGood = () => {
-    setGood(good + 1);
-    setTotal(total + 1);
-  };
+  const handleGood = () => setGood(good + 1);
 
-  const handleNeutral = () => {
-    setNeutral(neutral + 1);
-    setTotal(total + 1);
-  };
+  const handleNeutral = () => setNeutral(neutral + 1);
 
-  const handleBad = () => {
-    setBad(bad + 1);
-    setTotal(total + 1);
-  };
+  const handleBad = () => setBad(bad + 1);
 
   return (
     <div>
@@ -41,12 +42,7 @@ const App = () => {
       <Button text={"neutral"} onClick={handleNeutral} />
       <Button text={"bad"} onClick={handleBad} />
       <h2>statistics</h2>
-      <Display feedback={"good"} count={good} />
-      <Display feedback={"neutral"} count={neutral} />
-      <Display feedback={"bad"} count={bad} />
-      <Display feedback={"all"} count={total} />
-      <Display feedback={"average"} count={(good - bad) / total} />
-      <Display feedback={"positive"} count={100 * (good / total) + " %"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
