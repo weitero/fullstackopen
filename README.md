ffdebd56b3f9323e4c5815f070cdb38d9968af93
weitero <112836181+weitero@users.noreply.github.com>
2026-04-07 00:53:21 +0100

    refactor: extracting Filter, PersonForm, Persons, and Person components

--- README.md
+++ README.md
@@ -39,4 +39,4 @@
 - [x] 2.7: The Phonebook Step 2
 - [x] 2.8: The Phonebook Step 3
 - [x] 2.9*: The Phonebook Step 4
-- [ ] 2.10: The Phonebook Step 5
+- [x] 2.10: The Phonebook Step 5
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,52 @@
 import { useState } from "react";
 
+const Filter = (props) => {
+  return (
+    <form>
+      <div>
+        filter shown with <input value={props.value} onChange={props.onChange} />
+      </div>
+    </form>
+  );
+};
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <div>
+        name: <input value={props.nameValue} onChange={props.nameOnChange} />
+      </div>
+      <div>
+        number: <input value={props.numberValue} onChange={props.numberOnChange} />
+      </div>
+      <div>
+        <button type='submit'>add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons, newFilter }) => {
+  return persons
+    .filter((p) => {
+      if (newFilter === "") {
+        return true;
+      }
+      return p.name.toLocaleLowerCase().includes(newFilter.toLocaleLowerCase());
+    })
+    .map((p) => {
+      return <Person p={p} />;
+    });
+};
+
+const Person = ({ p }) => {
+  return (
+    <p key={p.name}>
+      {p.name} {p.number}
+    </p>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
   const [newName, setNewName] = useState("");
@@ -35,38 +82,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          filter shown with <input value={newFilter} onChange={handleFilterChange} />
-        </div>
-      </form>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons
-        .filter((p) => {
-          if (newFilter === "") {
-            return true;
-          }
-          return p.name.toLocaleLowerCase().includes(newFilter.toLocaleLowerCase());
-        })
-        .map((p) => {
-          return (
-            <p key={p.name}>
-              {p.name} {p.number}
-            </p>
-          );
-        })}
+
+      <Filter value={newFilter} onChange={handleFilterChange} />
+
+      <h3>add a new</h3>
+
+      <PersonForm
+        onSubmit={addPerson}
+        nameValue={newName}
+        nameOnChange={handleNameChange}
+        numberValue={newNumber}
+        numberOnChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
   );
 };
