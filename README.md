927e348b303d40e4259444d9b77b36ae0f80f868
weitero <112836181+weitero@users.noreply.github.com>
2026-04-06 21:43:24 +0100

    feat: use a Course component responsible for formatting a single course

--- README.md
+++ README.md
@@ -30,7 +30,7 @@
 
 ### Part 2
 
-- [ ] 2.1: Course information step 6
+- [x] 2.1: Course information step 6
 - [ ] 2.2: Course information step 7
 - [ ] 2.3*: Course information step 8
 - [ ] 2.4: Course information step 9
--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,30 @@
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Total = (props) => <p>Number of exercises {props.total}</p>;
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
     id: 1,
