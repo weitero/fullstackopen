5ccea084217f393b81084bdc066e6ee84251f3d5
weitero <112836181+weitero@users.noreply.github.com>
2026-04-09 00:38:04 +0100

    refactor: optimize length check for countries in Display component

--- part2/countries/src/components/Display.jsx
+++ part2/countries/src/components/Display.jsx
@@ -3,11 +3,12 @@ import Countries from "./Countries";
 import Country from "./Country";
 
 const Display = ({ countries, handleShowSingle }) => {
-  if (countries.length > 10) {
+  const n = countries.length;
+  if (n > 10) {
     return <Notification />;
-  } else if (countries.length > 1) {
+  } else if (n > 1) {
     return <Countries countries={countries} handleShowSingle={handleShowSingle} />;
-  } else if (countries.length === 1) {
+  } else if (n === 1) {
     return <Country country={countries[0]} />;
   }
 };
