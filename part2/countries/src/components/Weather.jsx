165e0e5f49271d131829ef5ed4e1197106ccb268
weitero <112836181+weitero@users.noreply.github.com>
2026-04-09 18:04:52 +0100

    feat: add weather icon display to Weather component

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -13,6 +13,9 @@ const Weather = ({ weather }) => {
     // </div>
     <div>
       <p>Temperature {weather.main.temp} Celsius</p>
+      <img
+        src={`https://openweathermap.org/payload/api/media/file/${weather.weather[0].icon}.png`}
+      />
       <p>Wind {weather.wind.speed} m/s</p>
     </div>
   );
