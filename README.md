fc943958ce21c5f53a1662549100e5ac42aed999
weitero <112836181+weitero@users.noreply.github.com>
2026-04-14 16:45:02 +0100

    feat: add POST functionality to create new persons in Phonebook backend

--- README.md
+++ README.md
@@ -57,7 +57,7 @@
 - [x] 3.2: Phonebook backend step 2
 - [x] 3.3: Phonebook backend step 3
 - [x] 3.4: Phonebook backend step 4
-- [ ] 3.5: Phonebook backend step 5
+- [x] 3.5: Phonebook backend step 5
 - [ ] 3.6: Phonebook backend step 6
 - [ ] 3.7: Phonebook backend step 7
 - [ ] 3.8*: Phonebook backend step 8
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -46,6 +48,28 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   persons = persons.filter((p) => p.id !== id);
   response.status(204).end();
+});
+
+const generateId = () => {
+  // Use a big enough range for your random values so that the likelihood of
+  // creating duplicate ids is small.
+  return String(Math.floor(Math.random() * 1_000_000_000));
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
