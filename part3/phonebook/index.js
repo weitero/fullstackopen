8a3b2c6a07a08b86504c97b3a48e931de89a11c4
weitero <112836181+weitero@users.noreply.github.com>
2026-04-19 21:59:01 +0100

    refactor: remove hardcoded persons array from index.js

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -11,29 +11,6 @@ morgan.token("data", (request, response) => {
 });
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
