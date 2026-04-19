42baa5773ed00754ebfee25379b1eead0aa95330
weitero <112836181+weitero@users.noreply.github.com>
2026-04-19 20:53:02 +0100

    feat: change the backend so that new numbers are saved to the database (ignore whether duplication exists for now)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -101,13 +101,15 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({ error: "name must be unique" });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: generateId(),
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  });
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT;
