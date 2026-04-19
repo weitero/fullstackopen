b6e23b714909292375a5ca0f99dabbf7e5ef8f59
weitero <112836181+weitero@users.noreply.github.com>
2026-04-19 23:30:37 +0100

    refactor: correct model import path for Person in index.js and add person model definition

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const Person = require("./models/note");
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.json());
