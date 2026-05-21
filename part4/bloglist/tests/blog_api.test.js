b38eaf48725e993913b3a50ae3853d13e80ed917
weitero <112836181+weitero@users.noreply.github.com>
2026-05-21 16:28:55 +0100

    feat: refactor GET endpoint to use async/await and add initial blog API tests

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  { title: 'Negative No More', author: 'Akio', url: 'http://nnm.com', likes: '2048' },
+  { title: 'A Tale of Two Cities', author: 'Josée', url: 'http://atotc.com', likes: '2048' },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test.only('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
